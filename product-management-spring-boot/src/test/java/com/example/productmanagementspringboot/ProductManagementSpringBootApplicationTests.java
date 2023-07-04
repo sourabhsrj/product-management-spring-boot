47eb52caff2d773eee9f4b4ec254dd5b246e9325
SourabhSrj <sourabhj053@gmail.com>
2023-07-04 21:32:48 +0530

    initial commit.... created spring boot project...!

---
+++ product-management-spring-boot/src/test/java/com/example/productmanagementspringboot/ProductManagementSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productmanagementspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
