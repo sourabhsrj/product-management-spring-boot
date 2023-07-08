8b6ae5370b87ae0a954ab5b8ecdea1a089c1c066
SourabhSrj <sourabhj053@gmail.com>
2023-07-08 21:41:29 +0530

    created and implemented pojo for common attributes...!

---
+++ product-management-spring-boot/src/main/java/com/example/productmanagementspringboot/model/CommmonAttributes.java
@@ -0,0 +1,66 @@
+package com.example.productmanagementspringboot.model;
+
+import java.util.Date;
+
+public class CommmonAttributes {
+
+    private String createdBy;
+
+    private Date createdOn;
+
+    private String updatedBy;
+
+    private Date updatedOn;
+
+    public CommmonAttributes() {
+    }
+
+    public CommmonAttributes(String createdBy, Date createdOn, String updatedBy, Date updatedOn) {
+        this.createdBy = createdBy;
+        this.createdOn = createdOn;
+        this.updatedBy = updatedBy;
+        this.updatedOn = updatedOn;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public Date getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(Date updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    @Override
+    public String toString() {
+        return "CommmonAttributes{" +
+                "createdBy='" + createdBy + '\'' +
+                ", createdOn=" + createdOn +
+                ", updatedBy='" + updatedBy + '\'' +
+                ", updatedOn=" + updatedOn +
+                '}';
+    }
+}
